811327860c6b3103bdd8735d120ebf2f0a91bb9d
dudasfig <112442570+dudasfig@users.noreply.github.com>
2024-11-29 22:43:05 -0300

    connection

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lista-de-tarefas-sandy-six.vercel.app/", // Verifique se esta URL está correta
+  baseURL: "http://localhost:3001", // Verifique se esta URL está correta
 });
 
 export default api;
